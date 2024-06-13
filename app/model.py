7d54aecc789f15d72e7fb01287802e9a34e2f59f
Khurshid <40022458+khurshiduktamov@users.noreply.github.com>
2024-06-14 01:47:57 +0200

    Path erros solution for Streamlit cloud

--- app/model.py
+++ app/model.py
@@ -1,12 +1,34 @@
-import pathlib
-from fastai.vision.all import *
+# import pathlib
+# from fastai.vision.all import *
 
-# Temporary redirect PosixPath to WindowsPath
-temp = pathlib.PosixPath
-pathlib.PosixPath = pathlib.WindowsPath
+# # Temporary redirect PosixPath to WindowsPath
+# temp = pathlib.PosixPath
+# pathlib.PosixPath = pathlib.WindowsPath
+
+# def load_model():
+#     model_path = Path('saved_model/pneumonia_classification_model.pkl')
+    
+#     # Ensure the path is a string
+#     model_path_str = str(model_path)
+    
+#     # Debugging: Print the path to ensure it's correct
+#     print(f"Loading model from: {model_path_str}")
+    
+#     # Load the model
+#     model = load_learner(model_path_str)
+    
+#     # Return the model
+#     return model
+
+# # Restore PosixPath
+# pathlib.PosixPath = temp
+
+
+# Streamlit try
+from fastai.learner import load_learner
 
 def load_model():
-    model_path = Path('saved_model/pneumonia_classification_model.pkl')
+    model_path = 'saved_model/pneumonia_classification_model.pkl'
     
     # Ensure the path is a string
     model_path_str = str(model_path)
@@ -20,5 +42,3 @@ def load_model():
     # Return the model
     return model
 
-# Restore PosixPath
-pathlib.PosixPath = temp
