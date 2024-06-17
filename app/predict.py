590d7a09e9424e3c085dbb11940892f249df2256
Khurshid <40022458+khurshiduktamov@users.noreply.github.com>
2024-06-17 13:21:34 +0200

    adding xray detector function

--- app/predict.py
+++ app/predict.py
@@ -4,3 +4,11 @@ def predict(model, img_path):
     img = PILImage.create(img_path)
     pred_class, pred_idx, probs = model.predict(img)
     return pred_class
+
+
+def is_xray(model, img_path):
+    img = PILImage.create(img_path)
+    pred_class, pred_idx, probs = model.predict(img)
+    # Check if the predicted class is in the Xray classes
+    xray_classes = ["PNEUMONIA", "NORMAL"]
+    return pred_class in xray_classes
