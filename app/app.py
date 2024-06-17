0e7d4f7d2afaaa2fb06ebd48e40c5598b0b82079
Khurshid <40022458+khurshiduktamov@users.noreply.github.com>
2024-06-17 13:56:06 +0200

    removing try and except block to check

--- app/app.py
+++ app/app.py
@@ -38,60 +38,6 @@
 #     st.write(f"Prediction: {label}")
 
 
-# import streamlit as st
-# from PIL import Image
-# from model import load_model, load_xray_detector
-# from predict import predict, is_xray
-# import pathlib
-
-# # Temporary redirect PosixPath to WindowsPath
-# temp = pathlib.PosixPath
-# pathlib.PosixPath = pathlib.WindowsPath
-
-# # Function to load models
-# @st.cache(allow_output_mutation=True)
-# def load_models():
-#     model = load_model()  # Load pneumonia prediction model
-#     xray_detector = load_xray_detector()  # Load X-ray detector model
-#     return model, xray_detector
-
-# st.title("Pneumonia Detection from Chest X-rays")
-# st.write("Upload a chest X-ray image to get a prediction.")
-
-# # Load models
-# model, xray_detector = load_models()
-
-# # Function to predict and display results
-# def predict_and_display_results(image):
-#     st.image(image, caption='Uploaded Image.', use_column_width=True)
-#     st.write("")
-#     st.write("Checking if the image is an X-ray...")
-
-#     # Save uploaded image temporarily
-#     img_path = "temp_image.jpg"
-#     image.save(img_path)
-
-#     if is_xray(xray_detector, img_path):
-#         st.write("Image is an X-ray. Classifying for pneumonia...")
-#         label = predict(model, img_path)
-#         st.write(f"Prediction: {label}")
-#     else:
-#         st.write("Uploaded image is not an X-ray. Please upload a chest X-ray image.")
-
-# # Handle file upload
-# uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-
-# if uploaded_file is not None:
-#     try:
-#         image = Image.open(uploaded_file)
-#         predict_and_display_results(image)
-#     except:
-#         st.write("Invalid image file. Please try again.")
-
-# # Restore PosixPath after prediction
-# pathlib.PosixPath = temp
-
-
 import streamlit as st
 from PIL import Image
 from model import load_model, load_xray_detector
@@ -136,13 +82,10 @@ def predict_and_display_results(image):
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    try:
-        image = Image.open(uploaded_file)
-        predict_and_display_results(image)
-    except OSError as e:
-        st.write("Invalid image file. Please try again.")
-    except Exception as e:
-        st.write(f"Error: {e}")
+    image = Image.open(uploaded_file)
+    predict_and_display_results(image)
+
+    # st.write("Invalid image file. Please try again.")
 
 # Restore PosixPath after prediction
 pathlib.PosixPath = temp
