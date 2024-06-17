3eb5bd4b2aecfbc34fc5b6662a7fd38b5f463874
Khurshid <40022458+khurshiduktamov@users.noreply.github.com>
2024-06-17 13:21:10 +0200

    Loading xray detector model

--- app/model.py
+++ app/model.py
@@ -1,29 +1,3 @@
-# import pathlib
-# from fastai.vision.all import *
-
-# # Temporary redirect PosixPath to WindowsPath
-# temp = pathlib.PosixPath
-# pathlib.PosixPath = pathlib.WindowsPath
-
-# def load_model():
-#     model_path = Path('saved_model/pneumonia_classification_model.pkl')
-    
-#     # Ensure the path is a string
-#     model_path_str = str(model_path)
-    
-#     # Debugging: Print the path to ensure it's correct
-#     print(f"Loading model from: {model_path_str}")
-    
-#     # Load the model
-#     model = load_learner(model_path_str)
-    
-#     # Return the model
-#     return model
-
-# # Restore PosixPath
-# pathlib.PosixPath = temp
-
-
 # Streamlit try
 from fastai.learner import load_learner
 import pathlib
@@ -47,3 +21,7 @@ def load_model():
     # Return the model
     return model
 
+def load_xray_detector():
+    xray_model_path = 'saved_model/xray_detector_model.pkl'
+    xray_model = load_learner(xray_model_path)
+    return xray_model
